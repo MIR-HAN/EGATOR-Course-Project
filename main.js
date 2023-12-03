2f7ced9033185d53764da4e1579a228f0fa55da7
MIR-HAN <mmirhanyorulmazz@gmail.com>
2023-12-03 12:49:36 +0100

    first commit

---
+++ main.js
@@ -0,0 +1,46 @@
+// change navbar styles on scroll //
+
+window.addEventListener('scroll', ()=> {
+    document.querySelector('nav').classList.toggle
+    ('window-scroll', window.scrollY > 0)
+});
+// show/hide faq answer //
+
+const faqs = document.querySelectorAll('.faq');
+
+faqs.forEach(faq => {
+    faq.addEventListener('click', () => {
+        faq.classList.toggle('open');
+
+// cahnge the icon//
+const icon = faq.querySelector('.faq-icon i');
+if (icon.className === 'fa-solid fa-plus') {
+    icon.className = "fa-solid fa-minus"
+}
+else {
+    icon.className = "fa-solid fa-plus"
+}
+
+    })
+})
+
+// show/hide navmenu ///
+
+const menu = document.querySelector(".nav-menu");
+const menuBtn = document.querySelector("#open-menu-btn");
+const closeBtn = document.querySelector("#close-menu-btn");
+
+menuBtn.addEventListener('click', () => {
+    menu.style.display = "flex";
+    closeBtn.style.display = "inline-block";
+    menuBtn.style.display = "none";
+})
+
+// Close nav menu ///
+
+const closeNav = ()=> {
+    menu.style.display = "none";
+    closeBtn.style.display = "none";
+    menuBtn.style.display = "inline-block";
+}
+closeBtn.addEventListener('click', closeNav)
